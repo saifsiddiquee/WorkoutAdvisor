f6f74d1e5e368182311b46a38a66d255c00140db
saifsiddique <saif.rafi07@gmail.com>
2018-10-09 16:46:36 +0600

    workout adviosr initial

---
+++ app/src/main/java/com/example/gnt/myfirstapp/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.gnt.myfirstapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
